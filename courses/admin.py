450578c07b15558595538bae49f4a4bffa302d7b
Your Name <you@example.com>
2021-06-02 13:53:40 +0600

    Fixed issues with subtopics listing

--- courses/admin.py
+++ courses/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+@admin.register(models.SubTopic)
+class SubtopicAdmin(admin.ModelAdmin):
+    list_display = ('text', 'topic__name')
+    search_fields = ('text',)
+    list_filter = ('topic__name',)
+
+
 admin.site.register(models.Course)
 admin.site.register(models.Section)
 admin.site.register(models.Topic)
